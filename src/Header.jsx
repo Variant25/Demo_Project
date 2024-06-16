34e486e36f3cfada6191c8294879239a99b4da9a
Variant25 <ayushp041@gmail.com>
2024-06-16 18:36:50 +0530

    responsiveness added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,33 +1,32 @@
 // src/Header.js
 import React from 'react';
-import './header.css'
- // Import the CSS file
- import logo from './whole.png'
- import searchlogo from './search-line.png'
+import './header.css'; // Import the CSS file
+import logo from './whole.png';
+import searchlogo from './search-line.png';
+
 const Header = () => {
   return (
-    <> <div className="header1-top">
-    <div className="header1-logo">
-      <img src={logo} alt="ATG.WORLD" />
-    </div>
-    <div className="header1-search">
- 
-      <input type="text" placeholder=" Search for your favorite groups in ATG" />
-    </div>
-    <div className="header1-account">
-      <a href="/create-account">Create account. <span>It's free!</span></a>
-    </div>
-  </div>
-    <header className="header1">
-     
-      <div className="header1-content">
-        <div className="header1-text">
-          <h1>Computer Engineering</h1>
-          <p>142,765 Computer Engineers follow this</p>
+    <>
+      <div className="header1-top">
+        <div className="header1-logo">
+          <img src={logo} alt="ATG.WORLD" />
+        </div>
+        <div className="header1-search">
+          <input type="text" placeholder="Search for your favorite groups in ATG" />
+        </div>
+        <div className="header1-account">
+          <a href="/create-account">Create account. <span>It's free!</span></a>
+          <button>Join the group</button>
         </div>
       </div>
-    </header>
-  
+      <header className="header1">
+        <div className="header1-content">
+          <div className="header1-text">
+            <h1>Computer Engineering</h1>
+            <p>142,765 Computer Engineers follow this</p>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
